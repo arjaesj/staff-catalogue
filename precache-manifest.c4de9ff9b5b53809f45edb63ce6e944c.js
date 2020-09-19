02326e9b66b33554d6648dcd9da37da223198340
Rommel Jay San Jose <arjae.sj@gmail.com>
2020-09-19 20:16:52 +0800

    Updates

---
+++ precache-manifest.c4de9ff9b5b53809f45edb63ce6e944c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac57cdeb18268d1a311e30051f7c33ff",
+    "url": "/staff-catalogue/index.html"
+  },
+  {
+    "revision": "210ba438ee6a9ea797ea",
+    "url": "/staff-catalogue/static/css/2.6d04c983.chunk.css"
+  },
+  {
+    "revision": "3aaaa8534ecb1fb5ed48",
+    "url": "/staff-catalogue/static/css/main.2d99aa98.chunk.css"
+  },
+  {
+    "revision": "210ba438ee6a9ea797ea",
+    "url": "/staff-catalogue/static/js/2.5b6beb6e.chunk.js"
+  },
+  {
+    "revision": "9a8a64d30a81695e7691dd446a703cba",
+    "url": "/staff-catalogue/static/js/2.5b6beb6e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3aaaa8534ecb1fb5ed48",
+    "url": "/staff-catalogue/static/js/main.f7550241.chunk.js"
+  },
+  {
+    "revision": "245e69fb64237d5fbbeb",
+    "url": "/staff-catalogue/static/js/runtime-main.b9787780.js"
+  }
+]);
